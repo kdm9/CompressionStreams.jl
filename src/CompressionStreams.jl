9479778bde17ebae7a4e45e32cc278a29fd9ee0f
Kevin Murray <spam@kdmurray.id.au>
2016-04-06 11:55:31 +1000

    whitespace fix

--- src/CompressionStreams.jl
+++ src/CompressionStreams.jl
@@ -9,15 +9,18 @@ const MAGIC_TYPES = Dict(
     UInt8[0x1f, 0x8b] => ZlibInflateInputStream
 )
 
+
 function cstream(filename::Union{ASCIIString, UTF8String})
     bs = BufferedInputStream(open(filename))
     return cstream(bs)
 end
 
+
 function cstream(io::IO)
     bs = BufferedInputStream(io)
     return cstream(bs)
 end
+
 
 function cstream(bs::BufferedInputStream)
     compress_type = detect_type(bs)
@@ -26,6 +29,7 @@ function cstream(bs::BufferedInputStream)
     end
     return compress_type(bs)
 end
+
 
 function detect_type(stream::BufferedInputStream)
     buf = Vector{UInt8}(256)
@@ -37,5 +41,6 @@ function detect_type(stream::BufferedInputStream)
     end
     return :Void
 end
+
 
 end # module
